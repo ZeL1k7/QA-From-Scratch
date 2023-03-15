e220939a54c5a6b416fc7df8395434fbca877d24
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-03-15 14:29:53 +0300

    Updated dataset to show  only best answer using score

--- src/utils.py
+++ src/utils.py
@@ -7,16 +7,24 @@ class QuestionDataset(torch.utils.data.Dataset):
     def __init__(self, questions_path: Path, answers_path: Path):
         self._question_df = pd.read_csv(questions_path, encoding="latin-1")
         self._question_df = self._question_df[["Id", "Title", "Body"]]
-        self._answer_df = pd.read_csv(answers_path, encoding="lating-1")
-        self._answer_df = self._answer_df[["Id", "ParentId", "Body"]]
-        self._dataset = self._question_df.merge(self._answer_df, how="left", left_on="Id", right_on="ParentId")
+        self._answer_df = pd.read_csv(answers_path, encoding="latin-1")
+        self._answer_df = self._answer_df[["Id", "ParentId", "Body", "Score"]]
+        self._dataset = self._question_df.merge(
+            self._answer_df,
+            how="left",
+            left_on="Id",
+            right_on="ParentId",
+        )
         self._dataset = self._dataset[self._dataset.Body_y.notna()]
 
     def __getitem__(self, idx):
         question = self._question_df.iloc[idx]
+        question_id = question.Id
         question = [question.Title, question.Body]
-        answers = self._dataset[self._dataset.Id_x == question.id].Body_y.values #
-        return question, answers
+        answers = self._dataset[self._dataset.Id_x == question_id]
+        answers = answers.sort_values(by="Score", ascending=False)
+        best_answer = answers.Body_y.values[0]
+        return question, best_answer
 
     def __len__(self):
         return len(self._question_df)
