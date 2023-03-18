b03ea4d5c1cb817e63affc2441116909f80d7531
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-03-19 02:32:31 +0300

    Replace argparse with typer

--- src/train_model.py
+++ src/train_model.py
@@ -1,22 +1,21 @@
-import argparse
-import math
+from pathlib import Path
 import torch
-from utils import QuestionDataset, load_model, load_tokenizer, create_index, get_n_splits, save_index
+import typer
+from utils import (
+    QuestionDataset,
+    load_model,
+    load_tokenizer,
+    create_index,
+    get_n_splits,
+    save_index,
+)
 from model import train_index, add_sentence_to_index
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("question_path", help="Path to question dataset")
-    parser.add_argument("answer_path", help="Path to answer dataset")
-    parser.add_argument("index_path", help="Path where save index")
-    parser.add_argument("batch_size", help="batch_size")
-    parser.add_argument("device", help="device")
-    args = parser.parse_args()
+def main(question_path: Path, index_path: Path, batch_size: int, device: str):
+    dataset = QuestionDataset(question_path)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size)
 
-    dataset = QuestionDataset(args.question_path, args.answer_path)
-    dataloader = torch.utils.data.DataLoader(dataset, args.batch_size)
-    device = args.device
     tokenizer = load_tokenizer()
     model = load_model(device)
 
@@ -40,4 +39,8 @@ if __name__ == "__main__":
             sentence=batch,
         )
 
-    save_index(index=index, index_path=args.index_path)
+    save_index(index=index, index_path=index_path)
+
+
+if __name__ == "__main__":
+    typer.run(main)
