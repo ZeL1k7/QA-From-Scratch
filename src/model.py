3d40f2f4c27943b80bc3b84f2d2475e88c2362db
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-03-18 20:34:22 +0300

    added sentence to index function

--- src/model.py
+++ src/model.py
@@ -31,9 +31,33 @@ def train_index(
         )
         sentence_embeddings = sentence_embeddings.detach().cpu().numpy()
         index_data[
-            idx * embedding_dim: (idx + 1) * embedding_dim
+            idx * embedding_dim : (idx + 1) * embedding_dim
         ] = sentence_embeddings
 
     index.train(index_data)
     return index
 
+
+def add_sentence_to_index(
+    index: faiss.IndexIVFFlat,
+    tokenizer: AutoTokenizer,
+    model: AutoModel,
+    sentence: str,
+    save_index: bool = True,
+) -> faiss.IndexIVFFlat:
+
+    encoded_input = tokenizer(
+        sentence,
+        truncation=True,
+        padding=True,
+        return_tensors="pt",
+    )
+    word_embeddings = model(encoded_input)
+    sentence_embeddings = mean_pooling(word_embeddings, encoded_input["attention_mask"])
+    sentence_embeddings = sentence_embeddings.detach().cpu().numpy()
+    index.add(sentence_embeddings)
+
+    if save_index:
+        faiss.write_index(index, "question.index")
+
+    return index
