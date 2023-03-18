66a605ae2a09a0a60ab5f253a7766b6eb3ca0650
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-03-18 22:40:53 +0300

    Get answer function

--- src/model.py
+++ src/model.py
@@ -1,39 +1,29 @@
+import pandas as pd
 import torch
 import faiss
 import numpy as np
 from transformers import AutoTokenizer, AutoModel
-from utils import mean_pooling
+from utils import mean_pooling, AnswerDataset, QuestionDataset, get_sentence_embedding
 
 
 def train_index(
     index: faiss.IndexIVFFlat,
     tokenizer: AutoTokenizer,
     model: AutoModel,
-    dataset: torch.utils.data.Dataset,
+    dataset: QuestionDataset,
     embedding_dim: int = 384,
     batch_size: int = 32,
 ) -> faiss.IndexIVFFlat:
-
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
     index_data = np.zeros(
         (embedding_dim * len(dataset), embedding_dim), dtype=np.float32
     )
 
     for idx, batch in enumerate(dataloader):
-        encoded_input = tokenizer(
-            batch,
-            truncation=True,
-            padding=True,
-            return_tensors="pt",
-        )
-        word_embeddings = model(**encoded_input)
-        sentence_embeddings = mean_pooling(
-            word_embeddings, encoded_input["attention_mask"]
-        )
-        sentence_embeddings = sentence_embeddings.detach().cpu().numpy()
+        sentence_embeddings = get_sentence_embedding(batch, tokenizer, model)
 
         index_data[
-            idx * embedding_dim: (idx + 1) * embedding_dim
+            idx * embedding_dim : (idx + 1) * embedding_dim
         ] = sentence_embeddings
 
     index.train(index_data)
@@ -47,19 +37,28 @@ def add_sentence_to_index(
     sentence: str,
     save_index: bool = True,
 ) -> faiss.IndexIVFFlat:
-
-    encoded_input = tokenizer(
-        sentence,
-        truncation=True,
-        padding=True,
-        return_tensors="pt",
-    )
-    word_embeddings = model(**encoded_input)
-    sentence_embeddings = mean_pooling(word_embeddings, encoded_input["attention_mask"])
-    sentence_embeddings = sentence_embeddings.detach().cpu().numpy()
+    sentence_embeddings = get_sentence_embedding(sentence, tokenizer, model)
     index.add(sentence_embeddings)
 
     if save_index:
         faiss.write_index(index, "question.index")
 
     return index
+
+
+def get_answer(
+    index: faiss.IndexIVFFlat,
+    answer_dataset: AnswerDataset,
+    question_dataset: QuestionDataset,
+    tokenizer: AutoTokenizer,
+    model: AutoModel,
+    sentence: str,
+    neighbors: int = 4,
+) -> pd.DataFrame:
+    query = get_sentence_embedding(sentence, tokenizer, model)
+    distances, question_idxs = index.search(query, neighbors)
+    answers_idxs = question_dataset.iloc[question_idxs[0]].Id.values
+    answer_df = pd.DataFrame()
+    for idx in answers_idxs:
+        answer_df = pd.concat([answer_df, answer_dataset.get_answer(idx)])
+    return answer_df
