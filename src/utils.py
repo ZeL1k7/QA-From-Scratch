df73be3e9728536b429ea5e6536c6e31354a5457
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-03-18 19:26:46 +0300

    Mean pooling function and change question dataset

--- src/utils.py
+++ src/utils.py
@@ -4,24 +4,29 @@ import pandas as pd
 
 
 class QuestionDataset(torch.utils.data.Dataset):
-    def __init__(self, questions_path: Path, answers_path: Path):
+    def __init__(self, questions_path: Path):
         self._question_df = pd.read_csv(questions_path, encoding="latin-1")
         self._question_df = self._question_df[["Id", "Title", "Body"]]
-        self._answer_df = pd.read_csv(answers_path, encoding="latin-1")
-        self._answer_df = self._answer_df[["Id", "ParentId", "Body", "Score"]]
-        self._dataset = self._question_df.merge(
-            self._answer_df,
-            how="left",
-            left_on="Id",
-            right_on="ParentId",
-        )
-        self._dataset = self._dataset[self._dataset.Body_y.notna()]
-        self._dataset = self._dataset.reset_index(drop=True)
 
     def __getitem__(self, idx):
-        question = self._question_df.iloc[idx]
-        question = [question.Title, question.Body]
+        question = self._question_df.iloc[idx].Title
         return question
 
     def __len__(self):
         return len(self._dataset)
+
+
+def mean_pooling(model_output: torch.FloatTensor, attention_mask: torch.BoolTensor) -> torch.FloatTensor:
+    """
+    Make sentence embedding averaging word embeddings
+    :param model_output:
+    :param attention_mask:
+    :return:
+    """
+    token_embeddings = model_output[0]
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    )
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
