568c8f5819020fa58757975ee87c86294bcf1bfe
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-03-19 18:08:40 +0300

    hash map realization for qa index

--- src/question_answer_index.py
+++ src/question_answer_index.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
+from datasets import AnswerDataset
 
 
 class IQAIndex(ABC):
@@ -13,3 +15,19 @@ class IQAIndex(ABC):
     @abstractmethod
     def get(self, item):
         ...
+
+
+class QAIndexHashMap(IQAIndex):
+    def __init__(self, dataset: AnswerDataset) -> None:
+        self._hash_map = defaultdict(list)
+        self._dataset = dataset
+
+    def build(self) -> None:
+        for idx, item in enumerate(self._dataset):
+            self._hash_map(item, idx)
+
+    def update(self, item: int, idx: int) -> None:
+        self._hash_map[item].append(idx)
+
+    def get(self, item: int) -> list[int]:
+        return self._hash_map[item]
