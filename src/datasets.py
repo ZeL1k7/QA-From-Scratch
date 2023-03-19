812feaf193f4e80601a7fac408951562f70a4bc4
Aleksey Malakhov <aleksey.malakhov11@gmail.com>
2023-03-19 17:21:47 +0300

    Update datasets

--- src/datasets.py
+++ src/datasets.py
@@ -4,33 +4,6 @@ from pydantic import BaseModel, Field
 import torch
 
 
-class Answer(BaseModel):
-    parent_id: int = Field(..., description="Id of parent question")
-    text: str = Field(..., description="Text of the answer")
-    score: int = Field(..., description="Score of the answer")
-
-
-class Answers(BaseModel):
-    items: List[Answer]
-
-
-class AnswerDataset:
-    def __init__(self, answers: Answers) -> None:
-        self.answers = answers
-
-    @classmethod
-    def from_json(cls, path: Path):
-        answers = Answers.parse_file(path)
-        return cls(answers=answers)
-
-    def __getitem__(self, idx):
-        item = self.answers.items[idx]
-        return item.parent_id[idx], item.title[idx], item.score[idx]
-
-    def __len__(self):
-        return len(self.answers.items)
-
-
 class Question(BaseModel):
     id: int = Field(..., description="Unique identifier")
     title: str = Field(..., description="Title of the question")
@@ -55,3 +28,31 @@ class QuestionDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.questions.items)
+
+
+class Answer(BaseModel):
+    id: int = Field(..., description="Id of answer")
+    parent_id: int = Field(..., description="Id of parent question")
+    text: str = Field(..., description="Text of the answer")
+    score: int = Field(..., description="Score of the answer")
+
+
+class Answers(BaseModel):
+    items: List[Answer]
+
+
+class AnswerDataset:
+    def __init__(self, answers: Answers) -> None:
+        self.answers = answers
+
+    @classmethod
+    def from_json(cls, path: Path):
+        answers = Answers.parse_file(path)
+        return cls(answers=answers)
+
+    def __getitem__(self, idx):
+        item = self.answers.items[idx]
+        return item.id, item.parent_id, item.title, item.score
+
+    def __len__(self):
+        return len(self.answers.items)
