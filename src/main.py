c7737b685c1f72d10ab843fcfb297a1fe6ee2481
Aleksey <aleksey.malakhov11@gmail.com>
2023-03-25 18:27:05 +0300

    Created factory

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import torch
 from fastapi import FastAPI
-from qa_index import get_answer, QAIndexHashMap
-from utils import load_model, load_tokenizer
-from vector_index import VectorIndexIVFFlat
+from qa_index import get_answer
+from factory import QAFactory
 
 from typing import Union
 
@@ -10,14 +9,21 @@ app = FastAPI()
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-tokenizer = load_tokenizer()
-model = load_model(device=device)
-index = VectorIndexIVFFlat.from_pretrained("data/vector.index")
-qa_index = QAIndexHashMap.from_pretrained("data/qa_index.pkl")
+
+factory = QAFactory(
+    vector_index_path="./app/data/vector.index",
+    qa_index_path="./app/data/qa_index.pkl",
+    device=device,
+)
+
+tokenizer = factory.create_tokenizer()
+model = factory.create_model()
+index = factory.create_vector_index()
+qa_index = factory.create_qa_index()
 
 
 @app.get("/send_answer")
-def send_answer(question: Union[str, list[str], list[list[str]]], num_answers: int):
+def send_answer(question: str, num_answers: int):
     answer = get_answer(
         index=index,
         qa_index=qa_index,
