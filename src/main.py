370c004f6e2e8b9982266c0eaacf932bb1bee860
Aleksey <aleksey.malakhov11@gmail.com>
2023-03-27 02:01:09 +0300

    Refactored code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-from typing import Union
-
 import torch
 from fastapi import FastAPI
 
 from factory import QAFactory
 from qa_index import get_answer
+
 
 app = FastAPI()
 
@@ -12,8 +11,8 @@ app = FastAPI()
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 factory = QAFactory(
-    vector_index_path="./app/data/vector.index",
-    qa_index_path="./app/data/qa_index.pkl",
+    vector_index_path="/app/data/vector.index",
+    qa_index_path="/app/data/qa_index.pkl",
     device=device,
 )
 
@@ -25,7 +24,7 @@ qa_index = factory.create_qa_index()
 
 @app.get("/send_answer")
 def send_answer(question: str, num_answers: int):
-    answer = get_answer(
+    return get_answer(
         index=index,
         qa_index=qa_index,
         tokenizer=tokenizer,
@@ -34,4 +33,3 @@ def send_answer(question: str, num_answers: int):
         sentence=question,
         neighbors=num_answers,
     )
-    return answer
